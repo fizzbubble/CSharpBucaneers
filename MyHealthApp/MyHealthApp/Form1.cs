646bf614ff81c57038a6784334c3121d5596abf5
lamfeng <40447844+lamfeng@users.noreply.github.com>
2018-10-31 14:47:41 +0800

    v1
    Co-Authored-By: fizzbubble <fizzbubble@users.noreply.github.com>

---
+++ MyHealthApp/MyHealthApp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MyHealthApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\MyHealthApp\MyHealthApp\MyHealthDB.mdf;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "You click the button!";
+
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM Customer";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter sa = new SqlDataAdapter(cmd);
+            sa.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
